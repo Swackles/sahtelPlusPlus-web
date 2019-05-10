41b2c71aca577d1fdf82da408ead82423a0d0a2f
Swackles <svenerik.rebane@gmail.com>
2019-05-10 16:04:35 +0300

    inital commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  require("./../api/tunniplaan")(null, (data, err) => {
+    if (err) throw Error(err);
+    else {
+      console.log(data);
+      res.render('index', { title: 'Tunniplaan', data: JSON.parse(data) });
+    }
+  });
+});
+
+module.exports = router;
